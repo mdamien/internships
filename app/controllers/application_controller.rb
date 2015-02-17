ec8ba2d40924047e6d6fca4d1d9ddc5c1c48e370
Damien <damienlog@gmail.com>
2015-02-17 13:14:43 +0000

    internships view one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
   def index
     @internships = Internship.all
+  end
+  
+  def view
+    @internship = Internship.find(params[:id])
   end
 end
