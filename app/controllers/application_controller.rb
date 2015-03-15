fd815385c133fa221349254593e2df7fc28e1726
Maxime <final7765@gmail.com>
2015-03-15 21:58:23 +0100

    Can search for internships by semesters.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,34 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  before_filter :set_search_query, :only => [:index]
+
   def index
     #Retrieving most recent year data from database by default
-    most_recent_year = Internship.maximum("year")
-    @internships = Internship.where("year = ?", most_recent_year)
-                       .order(year: :ASC)
-                       .order(semester: :DESC)
-                       .order(country: :ASC)
-                       .order(company: :ASC)
+    @internships = Internship.search(params)
+    @years = Internship.allInternshipYears.map { |i| i.year }
+    @query = {
+        from_year: params[:from_year],
+        to_year: params[:to_year],
+        from_semester: params[:from_semester],
+        to_semester: params[:to_semester]
+    }
   end
   
   def view
     @internship = Internship.find(params[:id])
   end
+
+  protected
+
+  #Default search parameters.
+  def set_search_query
+    most_recent_year = Internship.maximum("year")
+
+    #Adding missing parameters by default
+    params[:from_year] ||= most_recent_year
+    params[:to_year] ||= most_recent_year
+    params[:from_semester] ||= "P"
+    params[:to_semester] ||= "A"
+  end
+
 end
--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,3 +1,41 @@
 class Internship < ActiveRecord::Base
+  scope :from_year, -> (year) { where("year >= ?", year) }
+  scope :to_year, -> (year) { where("year <= ?", year) }
 
+  def self.order_internships(internships)
+    return internships
+               .order(year: :DESC)
+               .order(semester: :DESC)
+               .order(country: :ASC)
+               .order(company: :ASC)
+  end
+
+  def self.most_recent_internships
+    most_recent_year = Internship.maximum("year")
+    return order_internships(Internship.from_year(most_recent_year))
+  end
+
+  def self.search query
+    if query.has_key?(:from_year) && query.has_key?(:to_year) && query.has_key?(:from_semester) && query.has_key?(:to_semester)
+      internships = from_year(query[:from_year]).to_year(query[:to_year])
+
+      if query[:from_semester] == 'A'
+        #Removing spring internship of from_year.
+        internships = internships.where.not("semester LIKE '%P%' and year = ?", query[:from_year])
+      end
+      if query[:to_semester] == 'P'
+        #Removing fall internship of from_year.
+        internships = internships.where.not("semester LIKE '%A%' and year = ?", query[:to_year])
+      end
+
+      return order_internships(internships)
+    end
+
+    # Returning most recent internships if missing parameters.
+    return most_recent_internships
+  end
+
+  def self.allInternshipYears
+    return select(:year).distinct.order(year: :ASC)
+  end
 end
