7eb07b8ca167eda7e02504d1f6598e968bf46a7e
Maxime <final7765@gmail.com>
2015-04-21 14:49:20 +0200

    Typeahead on current internships working.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,20 @@ class ApplicationController < ActionController::Base
     #Retrieving most recent year data from database by default
     @internships = Internship.search(params)
     @years = Internship.all_internship_years.map { |i| i.year }
+
+    #Internship data values in JSON (countries, cities, companies)
+    @internship_data_json = {
+        "countries" => SortedSet.new,
+        "cities" => SortedSet.new,
+        "companies" => SortedSet.new
+    }
+
+    @internships.each do |i|
+      @internship_data_json["countries"].add(i.country)
+      @internship_data_json["cities"].add(i.city)
+      @internship_data_json["companies"].add(i.company)
+    end
+
   end
   
   def view
