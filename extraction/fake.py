458dcdac853a2f9ae0f86f26f21c3b3344edf23f
Maxime <final7765@gmail.com>
2015-05-10 16:04:20 +0200

    Improved fake.py.

--- extraction/fake.py
+++ extraction/fake.py
@@ -1,26 +1,28 @@
+# -*- coding: utf-8 -*-
+
 import csv
 
 from faker import Faker
 fake = Faker(locale="fr_FR")
 #fake.seed(42)
 
-myfile = open("data/fake.csv", 'w')
+myfile = open("data/fake2.csv", 'w')
 wr = csv.writer(myfile)
 wr.writerow([x.strip() for x in "num, addresse, branche_abbrev, filiere," \
         "company, description, etudiant, niveau," \
         "semestre, semestre_annee, sujet, tuteur, done,confidentiel, country, city,lat,lng".split(',')])
-for _ in range(500):
-    num = fake.random_int(min=0, max=9999)
+for i in range(20000):
+    num = i #PK : can't be random (no duplicates allowed)
     addresse = fake.street_address()+"\n"+str(fake.random_int(min=0, max=7000)) \
         +" "+fake.city()+"\n"+fake.country().upper()
-    branche_abbrev = fake.random_element(('GI','GM/GSM','GB'))
-    done = fake.random_element(('x', 'x',' '))
+    branche_abbrev = fake.random_element(('GI','GM', 'GSM','GB', 'GSU', 'GP'))
+    done = fake.random_element(('x', 'x', 'x', 'x', ' '))
     confidentiel = fake.random_element((' ', ' ',' ','x', ' '))
-    filiere = fake.sentence(nb_words=5)
+    filiere = fake.sentence(nb_words=3)
     company = fake.company()
     description = fake.text()
     etudiant = fake.name()
-    niveau = fake.random_element(('ouvrier','projet de fin d\'étude','stage ouvrier'))
+    niveau = fake.random_element(('assistant',u"projet de fin d\'étude",'stage ouvrier', 'apprentissage', 'interculturel'))
     country = fake.country().upper()
     city = fake.city()
     semestre = fake.random_element(('A','P'))
@@ -29,7 +31,7 @@ for _ in range(500):
     lat = fake.latitude()
     lng = fake.longitude()
     tuteur = fake.name()
-    wr.writerow((num, addresse, branche_abbrev, filiere,
-        company, description, etudiant, niveau,
-        semestre, semestre_annee, sujet, tuteur, done,
-        confidentiel, country, city,lat,lng))
+    wr.writerow((num, addresse.encode('utf-8'), branche_abbrev.encode('utf-8'), filiere.encode('utf-8'),
+        company.encode('utf-8'), description.encode('utf-8'), etudiant.encode('utf-8'), niveau.encode('utf-8'),
+        semestre.encode('utf-8'), semestre_annee, sujet.encode('utf-8'), tuteur.encode('utf-8'), done.encode('utf-8'),
+        confidentiel.encode('utf-8'), country.encode('utf-8'), city.encode('utf-8'),lat,lng))
